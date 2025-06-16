7bd71f9700be244dcf6597a2335505a833dca9b3
thecompanionnoob <97998469+thecompanionnoob@users.noreply.github.com>
2025-06-16 00:02:41 -0400

    STEP ONE

--- CompANSWR v1.0/common/defines.lua
+++ CompANSWR v1.0/common/defines.lua
@@ -1,7 +1,7 @@
 defines = {
 
-start_date = '1836.1.1',
-end_date = '1935.12.31',
+start_date = '2836.1.1',
+end_date = '2935.12.31',
 
 country = {
 	YEARS_OF_NATIONALISM 	= 20,   -- Years of Nationalism
